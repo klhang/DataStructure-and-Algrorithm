99d947b539cd852c65f1498d32fef5ecfa0c19eb
Klhang <chrisjunhang@gmail.com>
2018-04-23 17:18:49 -0700

    update

--- test.java
+++ test.java
@@ -1,36 +1,13 @@
-class test {
-  public static boolean search(int[] nums, int target) {
-      int start = 0, end = nums.length - 1;
-      while (start <= end){
-          int mid = start + (end - start)/ 2;
+import java.util.*;
 
-          if  (nums[mid] == target){
-              return true;
-          }
-
-          if  (nums[mid] < nums[end]){
-              if  (target > nums[mid] && target < nums[end] ){
-                  start = mid + 1;
-              } else {
-                  end = mid - 1;
-              }
-          } else if (nums[mid] > nums[end]) {
-              if  (target > nums[start] && target < nums[mid]){
-                  end = mid - 1;
-              } else {
-                  start = mid + 1;
-             }
-          } else {
-              end--;
-          }
-      }
-      return false;
-
-  }
+public class test{
 
 
   public static void main(String[] args){
-    boolean res = search(new int[]{1,3},3);
-    System.out.println(res);
+    List<Integer> list = new ArrayList<>();
+    list.add(0,1);
+    list.add(0,9);
+    list.add(-1);
+    System.out.println(list);
   }
 }
