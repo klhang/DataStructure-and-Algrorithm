f30ee2e8e940630c382ce1ef3542a962aa78ff20
Klhang <chrisjunhang@gmail.com>
2018-05-02 23:12:45 -0700

    update

--- test.java
+++ test.java
@@ -1,50 +1,7 @@
-class Solution {
-  public List<Integer> inorderTraversal(TreeNode root){
-    List<Integer> res = new ArrayList<Integer>();
-    Deque<TreeNode> deque = new ArrayDeque<>();
-
-    pushLeftNodes(root, deque);
-
-    while(!deque.isEmpty()){
-      TreeNode curr = deque.removeFirst();
-      res.add(curr.val);
-
-      pushLeftNodes(curr.right);
-    }
-
-    return res;
-  }
-
-  private void pushLeftNodes(TreeNode root, Deque<TreeNode> deque){
-    if (root != null) {
-      deque.addFirst(root);
-      root = root.left;
+public class test {
+    public static void main(String[] args) {
+      int[] test = new int[]{1,2,3};
+      test.remove(2);
+      System.out.println(test[2]);
     }
   }
-}
-
-class Solution {
-  public List<Integer> preorderTraversal(TreeNode root){
-    List<Integer> res = new ArrayList<>();
-
-    if (root = null) {
-      return res;
-    }
-
-    Deque<TreeNode> deque = new ArrayDeque<>();
-
-
-    while(!deque.isEmpty()){
-      TreeNode curr = deque.removeFirst();
-      res.add(curr.val);
-
-      if (curr.right != null) {
-        deque.addFirst(curr.right);
-      }
-
-      if (curr.left != null) {
-        deque.addFirst(curr.left);
-      }
-    }
-  }
-}
