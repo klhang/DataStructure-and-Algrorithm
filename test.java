0e84960fb40a6287a3f130e02fad2c5a0a50c09e
Klhang <chrisjunhang@gmail.com>
2018-05-04 16:58:16 -0700

    update

--- test.java
+++ test.java
@@ -1,7 +1,9 @@
+import java.util.*;
+
 public class test {
     public static void main(String[] args) {
       int[] test = new int[]{1,2,3};
-      test.remove(2);
-      System.out.println(test[2]);
+
+      System.out.println(IntStream.of(test).max());
     }
   }
