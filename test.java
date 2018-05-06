c12312d02d7d7548c3a333b2f81857f6774c2d38
Klhang <chrisjunhang@gmail.com>
2018-05-05 19:24:05 -0700

    update

--- test.java
+++ test.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-      List<Integer> test = new ArrayList<Integer>();
-      test.add(1);
-      test.add(2);
-      test.add(3);
-      test.remove(1);
-      System.out.println(test);
+      int[] test = new int[4];
+
+      System.out.println(test[3]);
     }
   }
