13186cafd3918e880a7cd2422c331aa623f5e3af
Klhang <chrisjunhang@gmail.com>
2018-06-10 16:57:53 -0700

    update

--- test.java
+++ test.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-      String test = "hello world";
-      System.out.println(test);
+
+      Set<Integer> skp = new HashSet<>(Arrays.asList(1,2,2,3,4));
+      System.out.println(skp);
     }
   }
